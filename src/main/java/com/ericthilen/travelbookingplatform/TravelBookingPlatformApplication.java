6431bf5d334c64492477926a1852a6be60d99bfb
Eric Thilen <ericthilen2003@gmail.com>
2026-06-24 20:42:14 +0200

    Initial Spring Boot project setup

---
+++ src/main/java/com/ericthilen/travelbookingplatform/TravelBookingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.ericthilen.travelbookingplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelBookingPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelBookingPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ericthilen/travelbookingplatform/TravelBookingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ericthilen.travelbookingplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelBookingPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
