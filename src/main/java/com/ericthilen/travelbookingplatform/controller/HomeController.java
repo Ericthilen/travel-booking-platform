104b75208e4605a084cf33f9b1e627ba80eae230
Eric Thilen <ericthilen2003@gmail.com>
2026-07-17 12:36:30 +0200

    Redesign home page

--- src/main/java/com/ericthilen/travelbookingplatform/controller/HomeController.java
+++ src/main/java/com/ericthilen/travelbookingplatform/controller/HomeController.java
@@ -1,13 +1,35 @@
 package com.ericthilen.travelbookingplatform.controller;
 
+import com.ericthilen.travelbookingplatform.model.Travel;
+import com.ericthilen.travelbookingplatform.service.TravelService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
 
+    private final TravelService travelService;
+
+    public HomeController(TravelService travelService) {
+        this.travelService = travelService;
+    }
+
     @GetMapping("/")
-    public String showHomepage() {
+    public String showHomepage(Model model) {
+        List<Travel> popularTravels =
+                travelService.getAllTravels()
+                        .stream()
+                        .limit(6)
+                        .toList();
+
+        model.addAttribute(
+                "popularTravels",
+                popularTravels
+        );
+
         return "index";
     }
 }
